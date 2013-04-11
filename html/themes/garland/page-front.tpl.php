b7af7e571fc28327172a00d4ed63d37cd05ecb77
Natalie Greco <natalie@koibot.com>
2013-04-11 00:03:41 -0700

    storing old files for safe keeping.

---
+++ html/themes/garland/page-front.tpl.php
@@ -0,0 +1,81 @@
+<?php
+// $Id: page.tpl.php,v 1.18.2.1 2009/04/30 00:13:31 goba Exp $
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+<title>The Vineyard Wineshop</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style/main.css" media="screen" />
+</head>
+<body>
+
+<div id="header">
+	<div id="header_width"> </div>
+</div>
+
+<!--
+<div id="navWrapper">
+	<div id="nav">
+    	<ul>
+           	<li><a href="#">Home</a></li>
+            <li><a href="#">Tastings</a></li>
+            <li><a href="#">Newsletter</a></li>
+            <li><a href="#">Directions/Map</a></li>
+        </ul>
+    </div>
+</div>
+-->
+
+<div class="clear"></div>
+
+<div id="main">
+	<div id="main_width">
+    	<div id="main_left">
+            <div id="welcome">
+            	<h1>Welcome</h1>
+                <p>Our wine shop is located in the Greenwood neighborhood of Seattle Washington, USA. We are a &ldquo;brick &amp; mortar&rdquo; shop with a wide variety of wines in stock. We specialize in Old World wines from classic wine regions, with a select representation of domestic and New World bottlings. We are competitively priced and knowledgeable, and we hope this site will prove useful for our customers or encourage you to visit our store for the first time.</p>
+                <p>Our expanded website will launch soon.</p>
+            </div>
+		</div>
+        
+        <div id="main_right">
+                <div style="padding: 1em;"></div>
+            	<img src="images/shop.jpg" width="670" height="340" border="0" alt="The Vineyard wine shop" />
+                <div style="padding: .5em;"></div>
+                <div id="call_box">
+                	<h2>Call</h2>
+                    <p>(206)706-7349</p>
+                </div>
+                <div id="location_box">
+                	<h2>Location</h2>
+                    <p>8218 Greenwood Ave. N<br />
+                    Seattle, WA 98103</p>
+                </div>
+                <div id="winterhrs_box">
+                	<h2>Winter Hours / Oct - June</h2>
+                    <p>Tuesday-Friday noon to 6:30 p.m.<br />
+                    Saturday noon to 6 p.m.</p>
+                </div>
+                <div id="summerhrs_box">
+                	<h2>Summer Hours / July - Sept</h2>
+                    <p>Wednesday-Friday noon to 6:30 p.m.<br />
+                    Saturday noon to 6 p.m.</p>
+                </div>
+                <div style="clear: both"></div>
+        </div>
+        
+    </div>
+</div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-5137594-2");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
